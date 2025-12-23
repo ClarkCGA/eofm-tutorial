b7b84cb9ee45def7d9b3f82d2f41f9d0bfeb4375
Rahebe Abedi <113472302+Rahebe22@users.noreply.github.com>
2025-12-23 12:23:08 -0500

    Modify time_embedding_dim and update comments
    Updated time_embedding_dim to 112 and adjusted filter sizes in comments to reflect new dimensions.

--- src/models/unet_att_d.py
+++ src/models/unet_att_d.py
@@ -157,7 +157,7 @@ class AdditiveAttentionBlock(nn.Module):
 class unet_att_d(nn.Module):
     def __init__(self, in_channels, filter_config, block_num, dropout_rate=0, 
                  dropout_type='traditional', upmode="deconv_2", use_skipAtt=False, 
-                 time_embedding_dim=128):
+                 time_embedding_dim=112):
         super(unet_att_d, self).__init__()
 
         self.in_channels = in_channels
@@ -174,37 +174,37 @@ class unet_att_d(nn.Module):
 
         # Contraction Path
         self.encoder_1 = ConvBlock(self.in_channels, filter_config[0], num_conv_layers=block_num[0],
-                                   drop_rate=dropout_rate, dropout_type=dropout_type)  # 64x256x256
+                                   drop_rate=dropout_rate, dropout_type=dropout_type)  # filter_config[0]x224x224
         self.encoder_2 = ConvBlock(filter_config[0], filter_config[1], num_conv_layers=block_num[1],
-                                   drop_rate=dropout_rate, dropout_type=dropout_type)  # 128x128x128
+                                   drop_rate=dropout_rate, dropout_type=dropout_type)  # filter_config[1]x112x112
         self.encoder_3 = ConvBlock(filter_config[1], filter_config[2], num_conv_layers=block_num[2],
-                                   drop_rate=dropout_rate, dropout_type=dropout_type)  # 256x64x64
+                                   drop_rate=dropout_rate, dropout_type=dropout_type)  # filter_config[2]x56x56
         self.encoder_4 = ConvBlock(filter_config[2], filter_config[3], num_conv_layers=block_num[3],
-                                   drop_rate=dropout_rate, dropout_type=dropout_type)  # 512x32x32
+                                   drop_rate=dropout_rate, dropout_type=dropout_type)  # filter_config[3]x28x28
         self.encoder_5 = ConvBlock(filter_config[3], filter_config[4], num_conv_layers=block_num[4],
-                                   drop_rate=dropout_rate, dropout_type=dropout_type)  # 1024x16x16
+                                   drop_rate=dropout_rate, dropout_type=dropout_type)  # filter_config[4]x14x14
         self.encoder_6 = ConvBlock(filter_config[4], filter_config[5], num_conv_layers=block_num[5],
-                                   drop_rate=dropout_rate, dropout_type=dropout_type)  # 2048x8x8
+                                   drop_rate=dropout_rate, dropout_type=dropout_type)  # filter_config[5]x7x7
         self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
 
         # Expansion Path
-        self.decoder_1 = UpconvBlock(filter_config[5], filter_config[4], upmode=upmode)  # 1024x16x16
+        self.decoder_1 = UpconvBlock(filter_config[5], filter_config[4], upmode=upmode)  # filter_config[4]x14x14
         self.conv1 = ConvBlock(filter_config[4] * 2, filter_config[4], num_conv_layers=block_num[4], 
                                drop_rate=dropout_rate, dropout_type=dropout_type)
 
-        self.decoder_2 = UpconvBlock(filter_config[4], filter_config[3], upmode=upmode)  # 512x32x32
+        self.decoder_2 = UpconvBlock(filter_config[4], filter_config[3], upmode=upmode)  # filter_config[3]x28x28
         self.conv2 = ConvBlock(filter_config[3] * 2, filter_config[3], num_conv_layers=block_num[3], 
                                drop_rate=dropout_rate, dropout_type=dropout_type)
 
-        self.decoder_3 = UpconvBlock(filter_config[3], filter_config[2], upmode=upmode)  # 256x64x64
+        self.decoder_3 = UpconvBlock(filter_config[3], filter_config[2], upmode=upmode)  # filter_config[2]x56x56
         self.conv3 = ConvBlock(filter_config[2] * 2, filter_config[2], num_conv_layers=block_num[2], 
                                drop_rate=dropout_rate, dropout_type=dropout_type)
 
-        self.decoder_4 = UpconvBlock(filter_config[2], filter_config[1], upmode=upmode)  # 128x128x128
+        self.decoder_4 = UpconvBlock(filter_config[2], filter_config[1], upmode=upmode)  # filter_config[1]x112x112
         self.conv4 = ConvBlock(filter_config[1] * 2, filter_config[1], num_conv_layers=block_num[1], 
                                drop_rate=dropout_rate, dropout_type=dropout_type)
 
-        self.decoder_5 = UpconvBlock(filter_config[1], filter_config[0], upmode=upmode)  # 64x256x256
+        self.decoder_5 = UpconvBlock(filter_config[1], filter_config[0], upmode=upmode)  # filter_config[0]x224x224
         self.conv5 = ConvBlock(filter_config[0] * 2, filter_config[0], num_conv_layers=block_num[0], 
                                drop_rate=dropout_rate, dropout_type=dropout_type)
 
@@ -216,84 +216,84 @@ class unet_att_d(nn.Module):
             self.Att5 = AdditiveAttentionBlock(F_g=filter_config[0], F_x=filter_config[0],
                                                F_inter=int(filter_config[0] / 2))
 
-        #self.classifier = nn.Conv2d(filter_config[0], n_classes, kernel_size=1, stride=1, padding=0)  # classNumx256x256
+        #self.classifier = nn.Conv2d(filter_config[0], n_classes, kernel_size=1, stride=1, padding=0)  # classNumx224x224
         self.output_conv = nn.Conv2d(filter_config[0], in_channels, kernel_size=1)  # Predict noise matching input
 
     #def forward(self, inputs):
     def forward(self, inputs, t): # Adding timestep for diffusion
         # set_trace()
-        e1 = self.encoder_1(inputs)  # batch size x 64 x 256 x 256
-        p1 = self.pool(e1)  # batch size x 64 x 128 x 128
+        e1 = self.encoder_1(inputs)  
+        p1 = self.pool(e1)  
+        e2 = self.encoder_2(p1)  
+        p2 = self.pool(e2)  
 
-        e2 = self.encoder_2(p1)  # batch size x 128 x 128 x 128
-        p2 = self.pool(e2)  # batch size x 128 x 64 x 64
+        e3 = self.encoder_3(p2) 
+        p3 = self.pool(e3)  
 
-        e3 = self.encoder_3(p2)  # batch size x 256 x 64 x 64
-        p3 = self.pool(e3)  # batch size x 256 x 32 x 32
+        e4 = self.encoder_4(p3)  
+        p4 = self.pool(e4)  
 
-        e4 = self.encoder_4(p3)  # batch size x 512 x 32 x 32
-        p4 = self.pool(e4)  # batch size x 1024 x 16 x 16
+        e5 = self.encoder_5(p4)  
+        p5 = self.pool(e5)  
 
-        e5 = self.encoder_5(p4)  # batch size x 1024 x 16 x 16
-        p5 = self.pool(e5)  # batch size x 1024 x 8 x 8
+        e6 = self.encoder_6(p5)  
 
-        e6 = self.encoder_6(p5)  # batch size x 2048 x 8 x 8
-
-        d6 = self.decoder_1(e6)  # batch size x 1024 x 16 x 16
-        
         # Adding timestep embedding to bottleneck
         t_emb = get_timestep_embedding(t, self.time_mlp[0].in_features).to(inputs.device)
         t_emb = self.time_mlp(t_emb).view(-1, e6.shape[1], 1, 1)
         e6 = e6 + t_emb
 
-        if self.use_skipAtt:
-            x5 = self.Att1(g=d6, x=e5)  # batch size x 1024 x 16 x 16
-            skip1 = torch.cat((x5, d6), dim=1)  # batch size x 2048 x 16 x 16
-        else:
-            skip1 = torch.cat((e5, d6), dim=1)  # batch size x 2048 x 16 x 16
-
-        d6_proper = self.conv1(skip1)  # batch size x 1024 x 16 x 16
-
-        d5 = self.decoder_2(d6_proper)  # batch size x 512 x 32 x 32
+        d6 = self.decoder_1(e6)  
+        
 
         if self.use_skipAtt:
-            x4 = self.Att2(g=d5, x=e4)  # batch size x 512 x 32 x 32
-            skip2 = torch.cat((x4, d5), dim=1)  # batch size x 1024 x 32 x 32
+            x5 = self.Att1(g=d6, x=e5)  
+            skip1 = torch.cat((x5, d6), dim=1)  
         else:
-            skip2 = torch.cat((e4, d5), dim=1)  # batch size x 1024 x 32 x 32
+            skip1 = torch.cat((e5, d6), dim=1) 
 
-        d5_proper = self.conv2(skip2)  # batch size x 512 x 32 x 32
+        d6_proper = self.conv1(skip1)  
 
-        d4 = self.decoder_3(d5_proper)  # batch size x 256 x 64 x 64
+        d5 = self.decoder_2(d6_proper)  
 
         if self.use_skipAtt:
-            x3 = self.Att3(g=d4, x=e3)  # batch size x 256 x 64 x 64
-            skip3 = torch.cat((x3, d4), dim=1)  # batch size x 512 x 64 x 64
+            x4 = self.Att2(g=d5, x=e4)  
+            skip2 = torch.cat((x4, d5), dim=1)  
         else:
-            skip3 = torch.cat((e3, d4), dim=1)  # batch size x 512 x 64 x 64
+            skip2 = torch.cat((e4, d5), dim=1)  
 
-        d4_proper = self.conv3(skip3)  # batch size x 256 x 64 x 64
+        d5_proper = self.conv2(skip2)  
 
-        d3 = self.decoder_4(d4_proper)  # batch size x 128 x 128 x 128
+        d4 = self.decoder_3(d5_proper)  
 
         if self.use_skipAtt:
-            x2 = self.Att4(g=d3, x=e2)  # batch size x 128 x 128 x 128
-            skip4 = torch.cat((x2, d3), dim=1)  # batch size x 256 x 128 x 128
+            x3 = self.Att3(g=d4, x=e3)  
+            skip3 = torch.cat((x3, d4), dim=1)  
         else:
-            skip4 = torch.cat((e2, d3), dim=1)  # batch size x 256 x 128 x 128
+            skip3 = torch.cat((e3, d4), dim=1)  
 
-        d3_proper = self.conv4(skip4)  # batch size x 128 x 128 x 128
+        d4_proper = self.conv3(skip3) 
 
-        d2 = self.decoder_5(d3_proper)  # batch size x 64 x 256 x 256
+        d3 = self.decoder_4(d4_proper)  
 
         if self.use_skipAtt:
-            x1 = self.Att5(g=d2, x=e1)  # batch size x 64 x 256 x 256
-            skip5 = torch.cat((x1, d2), dim=1)  # batch size x 128 x 256 x 256
+            x2 = self.Att4(g=d3, x=e2)  
+            skip4 = torch.cat((x2, d3), dim=1)  
         else:
-            skip5 = torch.cat((e1, d2), dim=1)  # batch size x 128 x 256 x 256
+            skip4 = torch.cat((e2, d3), dim=1)  
 
-        d2_proper = self.conv5(skip5)  # batch size x 64 x 256 x 256
+        d3_proper = self.conv4(skip4)  
 
-        #d1 = self.classifier(d2_proper)  # batch size x classNum x 256 x 256
+        d2 = self.decoder_5(d3_proper)  
+
+        if self.use_skipAtt:
+            x1 = self.Att5(g=d2, x=e1)  
+            skip5 = torch.cat((x1, d2), dim=1)  
+        else:
+            skip5 = torch.cat((e1, d2), dim=1)  
+
+        d2_proper = self.conv5(skip5)  
+
+        #d1 = self.classifier(d2_proper)  
         d1 = self.output_conv(d2_proper)   # Predicted noise (same shape as input)
         return d1
